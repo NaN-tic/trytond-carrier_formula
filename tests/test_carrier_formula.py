f68d867040dfba5c846acf6bae725b201dab0130
resteve <resteve@zikzakmedia.com>
2015-12-01 11:04:03 +0100

    Tests

--- tests/test_carrier_formula.py
+++ tests/test_carrier_formula.py
@@ -1,36 +1,18 @@
-#!/usr/bin/env python
-# This file is part of carrier_formula module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
-from trytond.tests.test_tryton import test_view, test_depends
-from trytond.tests.test_tryton import doctest_setup, doctest_teardown
-import trytond.tests.test_tryton
+# This file is part of the carrier_formula module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
-import doctest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class CarrierFormulaTestCase(unittest.TestCase):
+class CarrierFormulaTestCase(ModuleTestCase):
     'Test Carrier Formula module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module(
-            'carrier_formula')
-
-    def test0005views(self):
-        'Test views'
-        test_view('carrier_formula')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'carrier_formula'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         CarrierFormulaTestCase))
-    suite.addTests(doctest.DocFileSuite(
-            'scenario_carrier_formula.rst',
-            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
-            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
