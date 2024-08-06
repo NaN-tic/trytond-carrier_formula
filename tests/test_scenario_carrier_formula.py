5eb2a77046f64d5cf998ec0d31f9ad07e1abd283
Ismael Marcos Martínez <ismael.marcos@nan-tic.com>
2024-08-06 12:09:02 +0200

    Convert ‘test’ files to .py

---
+++ tests/test_scenario_carrier_formula.py
@@ -0,0 +1,133 @@
+import unittest
+from decimal import Decimal
+
+from proteus import Model
+from trytond.modules.account.tests.tools import (create_chart,
+                                                 create_fiscalyear, create_tax,
+                                                 get_accounts)
+from trytond.modules.account_invoice.tests.tools import \
+    set_fiscalyear_invoice_sequences
+from trytond.modules.company.tests.tools import create_company, get_company
+from trytond.tests.test_tryton import drop_db
+from trytond.tests.tools import activate_modules
+
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        drop_db()
+        super().setUp()
+
+    def tearDown(self):
+        drop_db()
+        super().tearDown()
+
+    def test(self):
+
+        # Install carrier_formula, purchase_shipment_cost and sale_shipment_cost
+        config = activate_modules(
+            ['carrier_formula', 'purchase_shipment_cost', 'sale_shipment_cost'])
+
+        # Create company
+        _ = create_company()
+        company = get_company()
+        currency = company.currency
+
+        # Reload the context
+        User = Model.get('res.user')
+        config._context = User.get_preferences(True, config.context)
+
+        # Create fiscal year
+        fiscalyear = set_fiscalyear_invoice_sequences(
+            create_fiscalyear(company))
+        fiscalyear.click('create_period')
+
+        # Create chart of accounts
+        _ = create_chart(company)
+        accounts = get_accounts(company)
+        revenue = accounts['revenue']
+        expense = accounts['expense']
+
+        # Create tax
+        tax = create_tax(Decimal('.10'))
+        tax.save()
+
+        # Create supplier
+        Party = Model.get('party.party')
+        supplier = Party(name='Supplier')
+        supplier.save()
+
+        # Create customer
+        Party = Model.get('party.party')
+        customer = Party(name='Customer')
+        customer.save()
+
+        # Create category
+        ProductCategory = Model.get('product.category')
+        category = ProductCategory(name='Category')
+        category.save()
+
+        # Create account categories
+        ProductCategory = Model.get('product.category')
+        account_category = ProductCategory(name="Account Category")
+        account_category.accounting = True
+        account_category.account_expense = expense
+        account_category.account_revenue = revenue
+        account_category.save()
+        account_category_tax, = account_category.duplicate()
+        account_category_tax.customer_taxes.append(tax)
+        account_category_tax.save()
+
+        # Create product
+        ProductUom = Model.get('product.uom')
+        ProductTemplate = Model.get('product.template')
+        Product = Model.get('product.product')
+        unit, = ProductUom.find([('name', '=', 'Unit')])
+        gram, = ProductUom.find([('name', '=', 'Gram')])
+        product = Product()
+        template = ProductTemplate()
+        template.name = 'Product'
+        template.account_category = account_category_tax
+        template.default_uom = unit
+        template.type = 'goods'
+        template.salable = True
+        template.list_price = Decimal('20')
+        product, = template.products
+        product.cost_price = Decimal('8')
+        template.save()
+        product, = template.products
+        carrier_product = Product()
+        carrier_template = ProductTemplate()
+        carrier_template.name = 'Carrier Product'
+        carrier_template.account_category = account_category_tax
+        carrier_template.default_uom = unit
+        carrier_template.type = 'service'
+        carrier_template.salable = True
+        carrier_template.list_price = Decimal('3')
+        carrier_product, = carrier_template.products
+        carrier_product.cost_price = Decimal('3')
+        carrier_template.save()
+        carrier_product, = carrier_template.products
+
+        # Create carrier
+        Carrier = Model.get('carrier')
+        FormulaPriceList = Model.get('carrier.formula_price_list')
+        carrier = Carrier()
+        party = Party(name='Carrier')
+        party.save()
+        carrier.party = party
+        carrier.carrier_product = carrier_product
+        carrier.carrier_cost_method = 'formula'
+        carrier.formula_currency = currency
+        for sequence, formula, price in (
+            (10, 'sale.total_amount > 100', Decimal(25)),
+            (10, 'sale.total_amount > 50', Decimal(10)),
+            (10, 'sale.total_amount > 0', Decimal(5)),
+        ):
+
+            line = FormulaPriceList(sequence=sequence,
+                                    formula=formula,
+                                    price=price)
+
+            carrier.formula_price_list.append(line)
+        carrier.save()
