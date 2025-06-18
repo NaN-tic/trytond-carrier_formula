7f60fe1ab5dd8fd45c1d6c0e98ad5611688927a2
Raimon Esteve <raimonesteve@gmail.com>
2025-06-18 13:44:48 +0200

    FIX Removing deprecated args from setup

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ from configparser import ConfigParser
 MODULE = 'carrier_formula'
 PREFIX = 'nantic'
 MODULE2PREFIX = {
-    'stock_origin',
-    'stock_origin_sale',
+    'stock_origin': 'nantic',
+    'stock_origin_sale': 'nantic',
 }
 
 
@@ -80,7 +80,7 @@ setup(name='%s_%s' % (PREFIX, MODULE),
         ],
     package_data={
         'trytond.modules.%s' % MODULE: (info.get('xml', [])
-            + ['tryton.cfg', 'locale/*.po', 'tests/*.rst']),
+            + ['tryton.cfg', 'view/*.xml', 'locale/*.po', 'tests/*.rst']),
         },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
